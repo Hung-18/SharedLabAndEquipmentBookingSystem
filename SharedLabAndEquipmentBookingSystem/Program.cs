72972ec12f420aaadff43f3dd64f4957dd8a1bd4
Vũ Đức Hưng <hungvd18@gmail.com>
2026-07-08 11:02:14 +0700

    commit: configuration Middleware in program.cs file

--- SharedLabAndEquipmentBookingSystem/Program.cs
+++ SharedLabAndEquipmentBookingSystem/Program.cs
@@ -1,4 +1,6 @@
+using API.Middleware;
 using Infrastructure.AppDbContext;
+using Microsoft.AspNetCore.Razor.Hosting;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection.Metadata.Ecma335;
 
@@ -9,20 +11,32 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll", builder =>
+    options.AddPolicy("AllowAll", policy =>
     {
-        builder.AllowAnyOrigin()
+        policy.AllowAnyOrigin()
                .AllowAnyHeader()
                .AllowAnyMethod();
     });
 });
 
 var app = builder.Build();
+
+// Nối middleware xử lý lỗi toàn cục 
+app.UseMiddleware<GlobalExceptionMiddleware>();
+
+// kích hoạt trigger ở trong db
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    await dbContext.Database.MigrateAsync();
+    await dbContext.EnsureDatabaseGuardsCreatedAsync();
+}
 
 
 // Configure the HTTP request pipeline.
@@ -35,17 +49,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors("AllowAll");
+
+app.UseAuthentication();
 app.UseAuthorization();
-
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-
-    await db.Database.MigrateAsync();
-
-    await db.EnsureDatabaseGuardsCreatedAsync();
-}
-
 
 app.MapControllers();
 
