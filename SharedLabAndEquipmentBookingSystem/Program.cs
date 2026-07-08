4ada6babeffef83200bc64d61d7e342da255567e
Vũ Đức Hưng <hungvd18@gmail.com>
2026-07-08 09:51:29 +0700

    add swagger

--- SharedLabAndEquipmentBookingSystem/Program.cs
+++ SharedLabAndEquipmentBookingSystem/Program.cs
@@ -1,5 +1,6 @@
 using Infrastructure.AppDbContext;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,12 +10,25 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
 
@@ -22,7 +36,17 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+    await db.Database.MigrateAsync();
+
+    await db.EnsureDatabaseGuardsCreatedAsync();
+}
+
 
 app.MapControllers();
 
 app.Run();
+
